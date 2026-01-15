9d541a11b9d07b76a536fb2b14cec7305cb00eef
nikunj-bottle <nikunjthapa01@gmail.com>
2026-01-15 12:18:03 -0500

    Add files via upload
    changes of jan 15

--- script.js
+++ script.js
@@ -6,5 +6,26 @@ xhttp.onreadystatechange = function(){
     if (this.readyState == 4 && this.status == 200) 
         data = JSON.parse(xhttp.responseText);
     console.log(data);
-    data.forEach(function(game)
+    data.forEach(function(game){
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let textData =
+    "<div class='game-title'>" + game.title + "</div>" + "<span>" +
+    "Publisher:" + game.publisher + "<br>" +
+    "Release Date:" + game.releaseDate + "<br>" +
+    "Needs Research:" +
+    "</span>";
+    
+    card.innerHTML = textData;
+
+    if(game.imgSrc){
+        card.style.backgroundImage = "url(" + game.imgSrc +")";
+    }
+
+    grid.appendChild(card);
+
+});
 }
+xhttp.open("GET", "games.json", true);
+xhttp.send();
