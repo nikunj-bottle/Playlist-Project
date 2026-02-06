eb8eb02906a869d5d9a34f7d24f4932aa2a32b3a
nikunj-bottle <nikunjthapa01@gmail.com>
2026-02-06 12:16:12 -0500

    Add files via upload
    2/6/2026

--- script.js
+++ script.js
@@ -1,48 +1,75 @@
 console.log("js console");
 var data;
 let grid = document.querySelector(".grid-container");
+// LOAD DATA (localStorage first, otherwise XHR)
+if (localStorage.getItem("datalist")) {
+  data = JSON.parse(localStorage.getItem("datalist"));
+  console.log("Loaded from localStorage");
+  if (grid) {
+    makeCards();
+  }
+} else {
+
 var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function(){
+
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-        data = JSON.parse(xhttp.responseText);
-    console.log(data);
-    data.forEach(function(game){
+      data = JSON.parse(this.responseText);
+      console.log("Loaded from gameData.json");
+
+      localStorage.setItem("datalist", JSON.stringify(data));
+      console.log("Saved starter data to localStorage");
+
+      if (grid) {
+        makeCards();
+      }
+    }
+  };
+
+  xhttp.open("GET", "games.json", true);
+  xhttp.send();
+}
+
+// RENDER CARDS
+function makeCards() {
+  grid.innerHTML = "";
+
+  data.forEach(function (game) {
     let card = document.createElement("div");
     card.classList.add("card");
 
     let textData =
     "<div class='game-title'>" + game.Game + "</div>" + "<span>" +
     "Publisher:" + game.Publisher + "<br>" +
-    "Release Date:" + game.Platform + "<br>" +
-    "Needs Research:" +
+    "Release Date:" + game.Year + "<br>" +
+  
     "</span>";
-    
     card.innerHTML = textData;
-
-    if(game.imgSrc){
-        card.style.backgroundImage = "url(" + game.imgSrc +")";
-    }
-
     grid.appendChild(card);
+  });
 
-});
+  console.log("cards refreshed");
 }
-}
-xhttp.open("GET", "games.json", true);
-xhttp.send();
+
+
+
+
+
+
+
+
 
 var form = document.querySelector("form");
-var titleInput = document.querySelector("#title-input");
-var pubInput = document.querySelector("#publisher-input");
-var dateInput = document.querySelector("#release-date-input");
+var titleInput = document.querySelector("#title");
+var dateInput = document.querySelector("#release-date");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   var newObj = {
-    title: titleInput.value,
-    publisher: pubInput.value,
-    releaseDate: dateInput.value
+    Game: titleInput.value,
+  
+    Year: dateInput.value
   };
 
   data.push(newObj);
@@ -56,3 +83,14 @@ form.addEventListener("submit", function (e) {
 
   form.reset();
 });
+
+// if(game.imgSrc){
+//         card.style.backgroundImage = "url(" + game.imgSrc +")";
+//     }
+
+    // let textData =
+    // "<div class='game-title'>" + game.Game + "</div>" + "<span>" +
+    // "Publisher:" + game.Publisher + "<br>" +
+    // "Release Date:" + game.Platform + "<br>" +
+    // "Needs Research:" +
+    // "</span>";
